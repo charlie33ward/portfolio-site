e95a9f5d643d25e75e6efc766f7612e8e796758f
Charlie Ward <charlie33ward@gmail.com>
2024-04-25 14:03:24 -0700

    Imported portfolio site I have been working on

---
+++ Portfolio/.eleventy.js
@@ -0,0 +1,5 @@
+﻿module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("tailwind.css");
+    eleventyConfig.addPassthroughCopy("/assets");
+    eleventyConfig.addPassthroughCopy("photos");
+}
